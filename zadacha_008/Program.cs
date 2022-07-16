23a1fc0cc71bb5961d44081b04c58b6d58a3b75f
Nadya <nadinru92@mail.ru>
2022-07-16 10:31:37 +0300

    Задача №8

---
+++ zadacha_008/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = new Random().Next(1, 100);
+    int i =1;
+    Console.WriteLine("Вывод числа: " + a);
+    while(a>=i){
+        if((i%2)==0){
+        Console.WriteLine(i);
+        }
+        i++;
+    }
