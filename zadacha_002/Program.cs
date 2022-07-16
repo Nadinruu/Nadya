7626fbf4123419c037c9c03f67dfcf6f21712549
Nadya <nadinru92@mail.ru>
2022-07-16 10:14:14 +0300

    задача №2

---
+++ zadacha_002/Program.cs
@@ -0,0 +1,8 @@
+﻿  int a = new Random().Next(1, 1000);
+    Console.WriteLine("Вывод числа: " + a);
+        if ((a%2)==0){
+        Console.WriteLine("Да");    
+        }
+        else{
+        Console.WriteLine("Нет");   
+        }
