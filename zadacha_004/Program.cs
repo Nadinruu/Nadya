e75a9032a502c3179a6ccaeb52348b348cc66e8f
Nadya <nadinru92@mail.ru>
2022-07-16 10:20:35 +0300

    Задача №4

---
+++ zadacha_004/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = new Random().Next(1,100);
+int b = new Random().Next(1,100);
+int c = new Random().Next(1,100);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);  
+if (a>b && a>c){
+    Console.WriteLine("Вывод максимального числа: "+a); 
+}
+else if (b>a && b>c){
+    Console.WriteLine("Вывод максимального числа: "+b);
+}
+else {
+    Console.WriteLine("Вывод максимального числа: "+c);
+}
