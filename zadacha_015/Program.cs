bb617be956046979bb3c7bbb6bbea8d2339c345a
Nadya <nadinru92@mail.ru>
2022-07-17 09:54:57 +0300

    Задача №15

---
+++ zadacha_015/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+   int f = 6;
+   int g =7;
+   if (f==number || g==number){
+      Console.WriteLine("{number} -> Да");}
+    else if (number > 7 || number < 1){
+    Console.WriteLine("Введите число от 1 до 7");
+    }
+    else {
+      Console.WriteLine($"{number} -> Нет");
+    }
