8c2944915c12c07732c5532b529388b592bc4314
Nadya <nadinru92@mail.ru>
2022-07-23 09:33:47 +0300

    Задача №23

---
+++ zadacha_023/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(1,10);
+Console.WriteLine ($"Вывод числа: {number} ");
+int i =1;
+    while (i <=number){
+      double q = Math.Pow(i,3);
+      Console.Write ($"{q} ");
+      i++;
+    }
