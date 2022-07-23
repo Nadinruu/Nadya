d812ce8eea8b43990589d0d29f241dbd6ac06c94
Nadya <nadinru92@mail.ru>
2022-07-23 09:17:56 +0300

    Задача №19

---
+++ zadacha_019/Program.cs
@@ -0,0 +1,16 @@
+﻿int inpnumber =12321;
+       int number =inpnumber;
+       int sum=0;
+       int i=0;
+       while(number>i){
+           int num2=number%10;
+           sum=num2*10+sum;
+           i++;
+       }
+        sum/=10;
+        if (inpnumber!=sum){
+            Console.WriteLine ($"{inpnumber}->Нет");
+        }
+        else {
+            Console.WriteLine ($"{inpnumber}->Да");
+        }
