694de073d742d54f304a2e46a9122da05abc645a
Nadya <nadinru92@mail.ru>
2022-07-24 08:50:30 +0300

    Задача №25

---
+++ zadacha_025/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(1,10);
+int st = new Random().Next(1,10);
+int i =1;
+int q =1;
+    while (i <=st){
+      q = number*q;
+      i++;
+    }
+    Console.Write ($"{number},{st}-> {q} ");
