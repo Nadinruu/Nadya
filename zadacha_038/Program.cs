cb9132b6e14886421d626fe5ec63501592e9f952
Nadya <nadinru92@mail.ru>
2022-07-30 09:10:53 +0300

    Задача №38

---
+++ zadacha_038/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = new int [] {1, 7, 6, 9, 21};
+int numMax = 0; 
+int numMin = numbers[0]; // присваиваем нулевой элемент массива
+for (int i = 0; i<numbers.Length; i++){
+	if (numbers[i]<numMin)
+	{
+		numMin = numbers[i]; 
+	}    
+	else{
+	    if (numbers[i]>numMax){
+		    numMax = numbers[i];
+	    }
+    }
+}
+ 
+Console.WriteLine("Вывести максимальное: " +numMax);
+Console.WriteLine("Вывести минимальное: " + numMin);
+numMax = numMax-numMin;
+Console.WriteLine("Разница: " + numMax);
