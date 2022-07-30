b747c5871405421597814fc65d4e78ecefaf7c19
Nadya <nadinru92@mail.ru>
2022-07-30 09:10:10 +0300

    Задача №36

---
+++ zadacha_036/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = new int [5];
+Random rand = new Random();
+int sum = 0;
+    for(int i=0; i<numbers.Length;i++){
+        numbers[i]= rand.Next(0,99);
+        Console.Write(numbers[i]+" ");
+        if(numbers[i]%2!=0){
+            sum+=numbers[i];
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Сумма чисел нечетных позиций: "+sum);
