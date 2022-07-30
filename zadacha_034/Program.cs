dc91857a1ceb11b181bde4cf328126a252c6cdb7
Nadya <nadinru92@mail.ru>
2022-07-30 09:08:32 +0300

    Задача №34

---
+++ zadacha_034/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numbers = new int [5];
+Random rand = new Random();
+int index = 0;
+    for(int i=0; i<numbers.Length;i++)
+    {
+        numbers[i]= rand.Next(100,1000);
+        Console.Write(numbers[i]+" ");
+        if(numbers[i]%2==0)
+        {
+            index++;
+        }
+        Console.WriteLine();
+    }
+     Console.WriteLine("Сумма четных чисел: "+index);
