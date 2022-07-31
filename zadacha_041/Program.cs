3f91a1c90f3d524553e37a42d84bbb120ebbfaa4
Nadya <nadinru92@mail.ru>
2022-07-31 09:49:40 +0300

    Задача №41

---
+++ zadacha_041/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("N =");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[n];
+int sum =0;
+	for(int i = 0; i < n; i++)
+    {
+		Console.Write($"Ввод с клаиватуры = ");
+		a[i] = Convert.ToInt32(Console.ReadLine());
+		if (a[i]>0)
+		{
+			sum+=1;	
+		}
+    }
+	Console.Write("Вывод суммы:"+sum);
