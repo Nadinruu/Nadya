6a5efd59a04f2a9244d16346729e85701bcca7b5
Nadya <nadinru92@mail.ru>
2022-08-06 01:32:47 +0300

    Задача №47

---
+++ zadacha_047/Program.cs
@@ -0,0 +1,37 @@
+﻿int m = IntIput("Введите колличество строк: ");
+int n = IntIput("Введите колличество столбцов: ");
+double [,] numbers = new double [m,n];
+
+FillArray(numbers);
+PrintArray (numbers);
+
+
+
+void PrintArray (double [,] array)
+{
+    for (int i =0; i < array.GetLength(0);i++){
+        for (int j =0; j< array.GetLength(1);j++){
+            Console.Write(Math.Round(array[i,j],1)+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void FillArray (double [,] array)
+{
+    for (int i =0; i < array.GetLength(0);i++){
+        for (int j =0; j< array.GetLength(1);j++){
+            array[i,j] = new Random().Next(-100,100) + new Random().NextDouble();
+        }
+    }
+}
+
+
+int IntIput (string output){ // выводит число в консоль
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine();
